a5ab0d73fb3d7f029cb832e2848980d92083e280
Asish Binu Mathew <asish@accubits.com>
2021-04-26 15:16:07 +0530

    Added button styling & positioning in gmail webapp

--- content.js
+++ content.js
@@ -1,15 +1,60 @@
 
 var uploadButton = document.createElement("BUTTON");
 var innerText = document.createTextNode("Upload Resume");
+uploadButtonStyle = `border-radius: 4px;
+                    border-width: 0px;
+                    height: 30px;
+                    background-color: #4285f4;
+                    color: white;
+                    `;
+uploadButtonClickedStyle = `border-radius: 4px;
+                            border-width: 0px;
+                            height: 30px;
+                            background-color: green;
+                            color: white;
+                            `;
+uploadButtonFailedStyle = `border-radius: 4px;
+                            border-width: 0px;
+                            height: 30px;
+                            background-color: red;
+                            color: white;
+                            `;
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "clicked_browser_action") {
-            uploadButton.appendChild(innerText);
-            document.getElementsByClassName("ha")[0].appendChild(uploadButton);
-            uploadButton.onclick = function () {
-                console.log("Do work here");
-            };
+            try {
+                uploadButton.appendChild(innerText);
+                document.getElementsByClassName("ha")[0].appendChild(uploadButton);
+                uploadButton.setAttribute("style", uploadButtonStyle);
+                uploadButton.onclick = function () {
+                    uploadButton.setAttribute("style", uploadButtonClickedStyle);
+                    uploadButton.innerHTML = "Uploading";
+                    setTimeout(processFuntion, 2000);
+                    console.log("Do work here");
+                };
+            }
+            catch {
+                console.log("Something bad happened :(");
+            }
+
         }
     }
 );
+
+
+function processFuntion() {
+    var current = new Date();
+    var a = current.getSeconds();
+    console.log(a);
+    if (a % 2 == 0) {
+        uploadButton.setAttribute("style", uploadButtonClickedStyle);
+        uploadButton.innerHTML = "Uploaded";
+    }
+    else {
+        uploadButton.setAttribute("style", uploadButtonFailedStyle);
+        uploadButton.innerHTML = "Upload Failed";
+    }
+
+
+}
