2861c090dc0ac170265616389618b28197dabec2
Asish Binu Mathew <asish@accubits.com>
2021-04-26 14:08:11 +0530

    "Added button to 3rd party webpage"

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-alert("hello from chrome extension")
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            var x = document.createElement("BUTTON");
+            var t = document.createTextNode("Upload Resume");
+            x.appendChild(t);
+            document.body.appendChild(x);
+
+        }
+    }
+);
+
